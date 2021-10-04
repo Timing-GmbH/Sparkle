1eb6bf9da70b27db2d2dace5fc77c8b559041dd5
Daniel Alm <Daniel.Alm@ForumD.net>
2021-10-04 14:23:18 +0200

    Suppress a particular deprecation message.

--- Sparkle/SUAppcastItem.h
+++ Sparkle/SUAppcastItem.h
@@ -339,7 +339,7 @@ SU_EXPORT @interface SUAppcastItem : NSObject<NSSecureCoding>
 + (instancetype)emptyAppcastItem;
 
 // Deprecated initializers
-- (nullable instancetype)initWithDictionary:(NSDictionary *)dict __deprecated_msg("Properties that depend on the system or application version are not supported when used with this initializer. The designated initializer is available in SUAppcastItem+Private.h. Please first explore other APIs or contact us to describe your use case.");
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dict /*__deprecated_msg("Properties that depend on the system or application version are not supported when used with this initializer. The designated initializer is available in SUAppcastItem+Private.h. Please first explore other APIs or contact us to describe your use case.")*/;
 - (nullable instancetype)initWithDictionary:(NSDictionary *)dict failureReason:(NSString * _Nullable __autoreleasing *_Nullable)error __deprecated_msg("Properties that depend on the system or application version are not supported when used with this initializer. The designated initializer is available in SUAppcastItem+Private.h. Please first explore other APIs or contact us to describe your use case.");
 - (nullable instancetype)initWithDictionary:(NSDictionary *)dict relativeToURL:(NSURL * _Nullable)appcastURL failureReason:(NSString * _Nullable __autoreleasing *_Nullable)error __deprecated_msg("Properties that depend on the system or application version are not supported when used with this initializer. The designated initializer is available in SUAppcastItem+Private.h. Please first explore other APIs or contact us to describe your use case.");
 
